63554962209cf3416d30f5904fb0f146d3886c20
Timothy H <timh1203@gmail.com>
2020-02-01 21:11:59 -0600

    Add logo and nav menu items

--- src/components/Home/Nav.js
+++ src/components/Home/Nav.js
@@ -1,6 +1,18 @@
+import Link from 'next/link'
+
 const Nav = () => (
   <main className="Nav">
-    <h1>Nav</h1>
+    <div className="nav-left">
+      <img src="/logo.png" alt='travelDev Logo' />
+    </div>
+    <div className="nav-right">
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/about"><a>About</a></Link>
+      <Link href="/playlists"><a>Playlists</a></Link>
+      <Link href="/discord"><a>Discord</a></Link>
+      <Link href="/blog"><a>Blog</a></Link>
+      <Link href="/resources"><a>Resources</a></Link>
+    </div>
   </main>
 )
 
