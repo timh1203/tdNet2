ee03a0a834f91224dda349ffc4174f7b3f63d3b2
Timothy H <timh1203@gmail.com>
2020-02-02 22:06:59 -0600

    Add basic styling to Header

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -3,15 +3,31 @@ import styled from 'styled-components'
 import ReactPlayer from 'react-player'
 
 const Header = () => (
-  <main className="Header">
+  <Main1 className="Header">
+    <H1A>travelDev</H1A>
     <ReactPlayer1
       url="https://youtu.be/EBjhGtDrEHk"
     />
-  </main>
+    <P1>I love to ️✈️, 💻, and 📹.</P1>
+  </Main1>
 )
 
+const Main1 = styled.main`
+  text-align: center;
+`
+const H1A = styled.h1`
+
+  font-weight: 800;
+  color: #292929;
+  text-shadow: 1px 4px 2px gray;
+  margin: 0;
+  font-size: 6rem;
+`
 const ReactPlayer1 = styled(ReactPlayer)`
-  margin: 0 auto;
+  margin: 2rem auto;
+`
+const P1 = styled.p`
+  font-size: 1.5rem;
 `
 
 export default Header
