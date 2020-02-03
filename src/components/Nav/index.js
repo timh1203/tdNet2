cd8e6a4b0f0e689656729c2a531add99ee8ef305
Timothy H <timh1203@gmail.com>
2020-02-03 12:07:25 -0600

    Add social icons and stylings

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,25 @@
 // PACKAGES
 import Link from 'next/link'
 import styled from 'styled-components'
+import { FaTwitterSquare, FaYoutubeSquare, FaInstagram } from 'react-icons/fa'
 
 const Nav = () => (
   <Main1 className="Nav">
     <Section1 className="nav-left">
       <img src="/logo.png" alt='travelDev Logo' />
     </Section1>
-    <Section2 className="nav-right">
+    <Section2 className="nav-middle">
+      <a href="www.youtube.com" target="_blank">
+        <FaYoutubeSquare1 size={50} />
+      </a>
+      <a href="www.twitter.com" target="_blank">
+        <FaTwitter1 size={50} />
+      </a>
+      <a href="www.instagram.com" target="_blank">
+        <FaInstagram1 size={43} />
+      </a>
+    </Section2>
+    <Section3 className="nav-right">
       <Link href="/">
         <A1>🏠 Home</A1>
       </Link>
@@ -26,18 +38,25 @@ const Nav = () => (
       <Link href="/resources">
         <A1>⚡️ Resources</A1>
       </Link>
-    </Section2>
+    </Section3>
   </Main1>
 )
 
 const Main1 = styled.main`
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `
 const Section1 = styled.section`
-  width: 40%;
+  width: 10%;
 `
 const Section2 = styled.section`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 20%;
+`
+const Section3 = styled.section`
   display: flex;
   justify-content: space-around;
   width: 60%;
@@ -52,6 +71,37 @@ const A1 = styled.a`
     cursor: pointer;
     text-decoration: underline;
     text-shadow: 0 0 20px #5454FF;
+  }
+`
+const FaYoutubeSquare1 = styled(FaYoutubeSquare)`
+  color: #FF0000;
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media (max-width: 375px) {
+    margin: 1rem;
+  }
+`
+const FaTwitter1 = styled(FaTwitterSquare)`
+  color: #0077B5;
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media (max-width: 375px) {
+    margin: 1rem;
+  }
+`
+const FaInstagram1 = styled(FaInstagram)`
+  color: white;
+  border-radius: 10%;
+  background: #d6249f;
+  background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%);
+  box-shadow: 0px 3px 10px rgba(0,0,0,.25);
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media (max-width: 375px) {
+    margin: 1rem;
   }
 `
 
