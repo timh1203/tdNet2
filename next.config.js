5cc2b447a50c71efdc9ca4ac7f3e3ff13151aaab
Timothy H <timh1203@gmail.com>
2020-02-07 16:35:20 -0600

    Add env variables, env example, gitignore

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const withFonts = require('next-fonts');
+
+require('dotenv').config()
+
 module.exports = withFonts({
+  env: {
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+    YOUTUBE_CHANNEL_ID: process.env.YOUTUBE_CHANNEL_ID
+  },
   webpack(config, options) {
     return config;
   }
