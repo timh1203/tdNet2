61abf0e2e02816052c26225a18eedea020aa3bad
Timothy H <timh1203@gmail.com>
2020-02-07 20:47:10 -0600

    Split social icons to separate component

--- src/components/libs/Nav/index.js
+++ src/components/libs/Nav/index.js
@@ -1,10 +1,9 @@
 // PACKAGES
 import Link from 'next/link'
 import styled from 'styled-components'
-import { FaTwitterSquare, FaYoutubeSquare, FaInstagram } from 'react-icons/fa'
 
 // LOCAL
-import { links } from '../../../../public'
+import Social from '../Social'
 
 const Nav = () => (
   <Main1 className="Nav">
@@ -12,15 +11,7 @@ const Nav = () => (
       <img src="/logo.png" alt='travelDev Logo' />
     </Section1>
     <Section2 className="nav-middle">
-      <a href={links.youtube.url} alt={links.youtube.alt} target="_blank">
-        <FaYoutubeSquare1 size={50} />
-      </a>
-      <a href={links.twitter.url} alt={links.twitter.alt} target="_blank">
-        <FaTwitter1 size={50} />
-      </a>
-      <a href={links.instagram.url} alt={links.instagram.alt} target="_blank">
-        <FaInstagram1 size={43} />
-      </a>
+      <Social />
     </Section2>
     <Section3 className="nav-right">
       <Link href="/">
@@ -74,37 +65,6 @@ const A1 = styled.a`
     cursor: pointer;
     text-decoration: underline;
     text-shadow: 0 0 20px #5454FF;
-  }
-`
-const FaYoutubeSquare1 = styled(FaYoutubeSquare)`
-  color: #FF0000;
-  &:hover {
-    transform: scale(1.2);
-  }
-  @media (max-width: 375px) {
-    margin: 1rem;
-  }
-`
-const FaTwitter1 = styled(FaTwitterSquare)`
-  color: #0077B5;
-  &:hover {
-    transform: scale(1.2);
-  }
-  @media (max-width: 375px) {
-    margin: 1rem;
-  }
-`
-const FaInstagram1 = styled(FaInstagram)`
-  color: white;
-  border-radius: 10%;
-  background: #d6249f;
-  background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%);
-  box-shadow: 0px 3px 10px rgba(0,0,0,.25);
-  &:hover {
-    transform: scale(1.2);
-  }
-  @media (max-width: 375px) {
-    margin: 1rem;
   }
 `
 
---
+++ src/components/libs/Social/index.js
@@ -0,0 +1,55 @@
+// PACKAGES
+import { FaTwitterSquare, FaYoutubeSquare, FaInstagram } from 'react-icons/fa'
+import styled from 'styled-components'
+
+// LOCAL
+import { links } from '../../../../public'
+
+const Social = () => (
+  <>
+    <a href={links.youtube.url} alt={links.youtube.alt} target="_blank">
+      <FaYoutubeSquare1 size={50} />
+    </a>
+    <a href={links.twitter.url} alt={links.twitter.alt} target="_blank">
+      <FaTwitter1 size={50} />
+    </a>
+    <a href={links.instagram.url} alt={links.instagram.alt} target="_blank">
+      <FaInstagram1 size={43} />
+    </a>
+  </>
+)
+
+
+const FaYoutubeSquare1 = styled(FaYoutubeSquare)`
+  color: #FF0000;
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media (max-width: 375px) {
+    margin: 1rem;
+  }
+`
+const FaTwitter1 = styled(FaTwitterSquare)`
+  color: #0077B5;
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media (max-width: 375px) {
+    margin: 1rem;
+  }
+`
+const FaInstagram1 = styled(FaInstagram)`
+  color: white;
+  border-radius: 10%;
+  background: #d6249f;
+  background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%);
+  box-shadow: 0px 3px 10px rgba(0,0,0,.25);
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media (max-width: 375px) {
+    margin: 1rem;
+  }
+`
+
+export default Social
