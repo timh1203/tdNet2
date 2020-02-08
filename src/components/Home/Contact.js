97f98add76000e21a9cfab31b4e4082510a0322a
Timothy H <timh1203@gmail.com>
2020-02-07 20:03:44 -0600

    Add basic contact fields

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -1,6 +1,28 @@
 const Contact = () => (
   <section>
-    <h1>Contact</h1>
+    <form>
+      <div className="form-group">
+        <label htmlFor="from"></label>
+        <input name="from" type="text" value="traveldev.net" hidden />
+      </div>
+      <div className="form-group">
+        <label htmlFor="firstName"></label>
+        <input name="firstName" type="text" placeholder="First Name" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="lastName"></label>
+        <input name="lastName" type="text" placeholder="Last Name" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="Email"></label>
+        <input name="Email" type="email" placeholder="Email" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="body"></label>
+        <textarea name="body" type="text" placeholder="Message" required />
+      </div>
+      <button type="submit">Send</button>
+    </form>
   </section>
 )
 
