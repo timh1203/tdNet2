4e0885bafc64b48b57fb574aeed14ebc19300536
Timothy H <timh1203@gmail.com>
2020-02-07 20:53:52 -0600

    Add basic content for footer

--- src/components/libs/Footer/index.js
+++ src/components/libs/Footer/index.js
@@ -1,6 +1,22 @@
+// LOCAL
+import Social from '../Social'
+import Menu from '../Menu'
+
+const About = () => {
+  return (
+    <>
+      <p>About</p>
+      <p>My personal website to share my journey in travel and web development. I love meeting new people, learning about tech advances of the future, and sharing outdoor adventures.</p>
+      <button>Read More</button>
+    </>
+  )
+}
+
 const Footer = () => (
   <main className="Footer">
-    <h1>Footer</h1>
+    <About />
+    <Social />
+    <Menu />
   </main>
 )
 
