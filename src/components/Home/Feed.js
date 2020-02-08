4a034ea84c4de2b5e691c0ecb3c5ffd1abc1a50d
Timothy H <timh1203@gmail.com>
2020-02-07 19:14:10 -0600

    Add more videos button link

--- src/components/Home/Feed.js
+++ src/components/Home/Feed.js
@@ -1,3 +1,6 @@
+// PACKAGES
+import styled from 'styled-components'
+
 // LOCAL
 import Youtube from '../../apis/youtube.js'
 
@@ -5,7 +8,16 @@ const Feed = () => (
   <main className="Feed">
     <h1>Latest Videos</h1>
     <Youtube />
+    <Div1>
+      <a href="https://www.youtube.com/channel/UCUCXUCn0PEPrp7nAIFpFbOw" target="_blank">
+        <button>More Videos</button>
+      </a>
+    </Div1>
   </main>
 )
+
+const Div1 = styled.div`
+  text-align: center;
+`
 
 export default Feed
