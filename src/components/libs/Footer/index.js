20ab91af2a087b156b6dc1926c36c47db475e293
Timothy H <timh1203@gmail.com>
2020-02-07 22:04:26 -0600

    Add footer styling

--- src/components/libs/Footer/index.js
+++ src/components/libs/Footer/index.js
@@ -1,23 +1,69 @@
+// PACKAGES
+import styled from 'styled-components'
+
 // LOCAL
 import Social from '../Social'
 import Menu from '../Menu'
 
 const About = () => {
   return (
-    <>
-      <p>About</p>
+    <Div1>
+      <P1>About</P1>
       <p>My personal website to share my journey in travel and web development. I love meeting new people, learning about tech advances of the future, and sharing outdoor adventures.</p>
       <button>Read More</button>
-    </>
+    </Div1>
   )
 }
 
 const Footer = () => (
-  <main className="Footer">
-    <About />
-    <Social />
-    <Menu />
-  </main>
+  <Main1>
+    <Section1>
+      <About />
+    </Section1>
+    <Section2>
+      <P1>Navigation</P1>
+      <Menu />
+    </Section2>
+    <Section3>
+      <P1>Social</P1>
+      <Div2>
+        <Social />
+      </Div2>
+    </Section3>
+  </Main1>
 )
+
+const Main1 = styled.main`
+  display: flex;
+  justify-content: space-around;
+
+`
+const Section1 = styled.section`
+  width: 40%;
+  text-align: center;
+`
+const Div1 = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`
+const P1 = styled.p`
+  font-size: 1.5rem;
+  font-weight: 800;
+`
+const Section2 = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 30%;
+  `
+const Section3 = styled.section`
+  width: 30%;
+  text-align: center;
+`
+const Div2 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`
 
 export default Footer
