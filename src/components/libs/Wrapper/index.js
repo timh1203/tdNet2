8a8430cad406750d036ffa2c3185579e2da36d13
Timothy H <timh1203@gmail.com>
2020-02-09 14:14:37 -0600

    Removed footer offset

--- src/components/libs/Wrapper/index.js
+++ src/components/libs/Wrapper/index.js
@@ -1,6 +1,5 @@
 // PACKAGES
 import React from 'react'
-import styled from 'styled-components'
 
 // LOCAL
 import Nav from '../Nav'
@@ -9,23 +8,11 @@ import Footer from '../Footer'
 const Wrapper = ({ children }) => {
   return (
     <main>
-      <Div1>
-        <Nav />
-        <Div2>
-          {children}
-        </Div2>
-      </Div1>
+      <Nav />
+      {children}
       <Footer />
     </main>
   )
 }
 
-const Div1 = styled.div`
-  min-height: 100%;
-  margin-bottom: 288px;
-`
-const Div2 = styled.div`
-  margin: 3rem auto;
-  width: 80%;
-`
 export default Wrapper
