c6905abfdfc71ba13caaeb5d5735b5fac2924faa
Timothy H <timh1203@gmail.com>
2020-02-12 20:56:26 -0600

    Add Playlist section

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+export const playlists = [
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjUVhr5FSvSM3CCNVBIx1t4g",
+    src: "https://i.imgur.com/ilpIY1U.png",
+    alt: "Web Development Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjWgUWNBu-pK-qX5rD3-DDde",
+    src: "https://i.imgur.com/hkhJeV3.png",
+    alt: "Lambda School Vlogs Playlist"
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjU0njRf536hVFx9-7cwBtG1",
+    src: "https://i.imgur.com/RtVxIwI.png",
+    alt: "Colombia Vlogs Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjXNL6SvDuG0wE7tMehCv5vz",
+    src: "https://i.imgur.com/o1UDMId.png",
+    alt: "Japan Vlogs Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjUrckwVyTFvN6rOKAbncwLp",
+    src: "https://i.imgur.com/t4ysr80.png",
+    alt: "Mexico Vlogs Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjVL7MGjNkvzhhLR5-V4cgi7",
+    src: "https://i.imgur.com/2XilNv2.png",
+    alt: "USA Vlogs Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjWBGlF15M5W02Lks5nHUnD0",
+    src: "https://i.imgur.com/sMQXzL6.png",
+    alt: "Announcements Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjU6Bxp-SExrh6Mu9LT3Mtv0",
+    src: "https://i.imgur.com/AqseJHK.png",
+    alt: "Mastermind Meets Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjXJ894_CtqtDWn3IMiH96R9",
+    src: "https://i.imgur.com/lSe1ArX.png",
+    alt: "Assorted Videos Playlist"
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjV0G9rR0mRZI51No2i9pJRE",
+    src: "https://i.imgur.com/kE1riOT.png",
+    alt: "Interviews Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjUvv5xH8AqMBXDbLR2APWZp",
+    src: "https://i.imgur.com/vVQj52E.png",
+    alt: "Hobbies Playlist",
+  },
+  {
+    href: "https://www.youtube.com/playlist?list=PLPTS2VjPOpjXSlyPlkjLmvG2ff51Kq3bS",
+    src: "https://i.imgur.com/r11sXso.png",
+    alt: "Travel Inspiration Music Playlist"
+  },
+]
--- src/components/Playlists/index.js
+++ src/components/Playlists/index.js
@@ -1,11 +1,42 @@
+// PACKAGES
+import styled from 'styled-components'
+
+// LOCAL
+import { playlists } from '../../assets'
+
+const createPlaylistItem = (item, index) => (
+  <article key={index}>
+    <a href={item.href} rel="noopener noreferrer" target="_blank">
+      <Img1 src={item.src} alt={item.alt} />
+    </a>
+  </article>
+)
+
 class Playlists extends React.Component {
   render() {
     return (
-      <main className="Playlists">
-        <h1>Playlists Page coming soon...</h1>
-      </main>
+      <Main1>
+        <Div1>
+          {playlists.map((item, index) => createPlaylistItem(item, index))}
+        </Div1>
+      </Main1>
     )
   }
 }
+
+const Main1 = styled.main`
+  width: 90%;
+  margin: 0 auto;
+`
+const Div1 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  flex-wrap: wrap;
+`
+const Img1 = styled.img`
+  max-height: 200px;
+  margin: 1rem;
+`
 
 export default Playlists;
