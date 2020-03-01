3fae07c7ed6cd19b63c246d0f267259d10248fa9
Timothy H <timh1203@gmail.com>
2020-02-29 20:39:00 -0600

    Add first pass for gear section

--- src/assets/index.js
+++ src/assets/index.js
@@ -60,3 +60,384 @@ export const playlists = [
     alt: "Travel Inspiration Music Playlist"
   },
 ]
+
+export const resources = {
+  desktop: [
+    {
+      title: "PC Case",
+      name: "Phanteks Eclipse P300 PH-EC300PTG_BK Black Steel Chassis, Tempered Glass Window ATX Mid Tower Computer Case",
+      desc: "Mid tower case with glass window and fits all the PC components.",
+      img: "",
+      link: "https://www.amazon.com/Phanteks-Eclipse-Steel-Tower-Tempered/dp/B075DMYVBV/ref=sr_1_1?keywords=Phanteks+Eclipse+P300+PH-EC300PTG_BK+Black+Steel+Chassis%2C+Tempered+Glass+Window+ATX+Mid+Tower+Computer+Case&qid=1583026483&sr=8-1",
+    },
+    {
+      title: "Motherboard",
+      name: "MSI MPG X570 Gaming Edge WiFi Motherboard (AMD AM4, DDR4, PCIe 4.0, SATA 6Gb/s, M.2, USB 3.2 Gen 2, AC Wi-Fi 5, HDMI, ATX)",
+      desc: "A great mobo with built in WiFi Card, Bluetooth, and plays well with the AMD Ryzen family.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07T2FWCYG/ref=ppx_yo_dt_b_asin_title_o02_s03?ie=UTF8&psc=1",
+    },
+    {
+      title: "CPU",
+      name: "AMD RYZEN 5 3600 6-Core 3.6 GHz (4.2 GHz Max Boost) Socket AM4 65W 100-100000031BOX Desktop Processor",
+      desc: "Smooth performance and designed with the gamer in mind with AMD Wraith Stealth Cooler fan.",
+      img: "",
+      link: "https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=sr_1_1?keywords=AMD+RYZEN+5+3600+6-Core+3.6+GHz+%284.2+GHz+Max+Boost%29+Socket+AM4+65W+100-100000031BOX+Desktop+Processor&qid=1583026084&sr=8-1",
+    },
+    {
+      title: "GPU",
+      name: "MSI Gaming GeForce RTX 2060 Super 8GB GDRR6 256-bit HDMI/DP G-SYNC Turing Architecture Overclocked Graphics Card (RTX 2060 Super Gaming X)",
+      desc: "Nvidia Turing architecture and next-generation performance with VR compatibility.",
+      img: "",
+      link: "https://www.amazon.com/MSI-Gaming-RTX-2060-Super/dp/B07V1Q4L2Z/ref=sr_1_2?keywords=MSI+GeForce+RTX+2060+SUPER+GAMING+x&qid=1583026228&sr=8-2",
+    },
+    {
+      title: "RAM",
+      name: "G.Skill 16GB (2 x 8GB) Ripjaws V Series DDR4 PC4-25600 3200MHz Intel Z170 Platform Desktop Memory F4-3200C16D-16GVRB",
+      desc: "After some faulty ram I received, this one worked like a charm.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B017RKV4N2/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Hard Drive",
+      name: "Samsung 860 QVO 1TB Solid State Drive (MZ-76Q1T0) V-NAND, SATA 6Gb/s, Quality and Value Optimized SSD",
+      desc: "An awesome solid state drive to make everything run butter smooth.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07L3D19MY/ref=ppx_yo_dt_b_asin_title_o02_s04?ie=UTF8&psc=1",
+    },
+    {
+      title: "Power Supply",
+      name: "Corsair CX Series 650 Watt 80 Plus Bronze Certified Modular Power Supply (CP-9020103-NA) ",
+      desc: "A great power supply with thermal controlled fans.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B01B72W1VA/ref=ppx_yo_dt_b_asin_title_o02_s01?ie=UTF8&psc=1",
+    },
+    {
+      title: "Monitor",
+      name: "Asus VG278Q 27 Full HD 1080P 144Hz 1ms Eye Care G-Sync Compatible Adaptive Sync Gaming Monitor with DP HDMI DVI",
+      desc: "A nice monitor with great resolution with slightly lower refresh rate for the right price.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B074JLD4HZ/ref=ppx_yo_dt_b_asin_title_o02_s02?ie=UTF8&psc=1",
+    },
+    {
+      title: "Keyboard",
+      name: "PICTEK TKL Mechanical Gaming Keyboard, Compact 87 Key RGB Computer Keyboard with Blue Equivalent Switches, 27 LED Lighting Modes, 100% Anti-Ghosting Wired Ergonomic Keyboard for Windows PC/MAC Games",
+      desc: "A budget keyboard with clicky keys and beautiful RGB patterns.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07VNQQZFK/ref=ppx_yo_dt_b_asin_title_o02_s04?ie=UTF8&psc=1",
+    },
+    {
+      title: "Mouse",
+      name: "PICTEK Gaming Mouse Wired, 8 Programmable Buttons, Chroma RGB Backlit, 7200 DPI Adjustable, Comfortable Grip Ergonomic Optical PC Computer Gaming Mice with Fire Button, Sega Genesis Accessories ",
+      desc: "Adjustable gaming mouse with enough buttons and RGB colors.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07G387ZJM/ref=ppx_yo_dt_b_asin_title_o02_s03?ie=UTF8&psc=1",
+    },
+    {
+      title: "Webcam",
+      name: "PC Webcam for Streaming HD 1080P, Vitade 960A USB Pro Computer Web Camera Video Cam for Mac Windows Laptop Conferencing Gaming Xbox Skype OBS Twitch Youtube Xsplit GoReact with Microphone & Ring Light ",
+      desc: "A webcam with small ring lighting and great resolution.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07RRZQBRN/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Headset",
+      name: "RUNMUS Gaming Headset Xbox One Headset with 7.1 Surround Sound Stereo, PS4 Headset with Noise Canceling Mic & LED Light, Compatible with PC, PS4, Xbox One Controller(Adapter Not Included)",
+      desc: "Budget quality headphones with LED lights and great microphone.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07GRM747Y/ref=ppx_yo_dt_b_asin_title_o02_s03?ie=UTF8&psc=1",
+    },
+    {
+      title: "Microphone",
+      name: "Blue Yeti Blackout + Assassin's Creed Odyssey Bundle",
+      desc: "One of the most popular microphones, albeit heavier, that came with AC Odyssey.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B00D5TOV76/ref=ppx_yo_dt_b_asin_title_o01_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Microphone Stand",
+      name: "Knox Gear Professional Microphone Studio Stand for Yeti and Snowball Microphones (Compatible with all Microphones and Shock Mounts)",
+      desc: "Holds up the Blue Yeti microphone securely.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07D7JTR5M/ref=ppx_yo_dt_b_asin_title_o00_s01?ie=UTF8&psc=1",
+    },
+    {
+      title: "Microphone Screen",
+      name: "YOUSHARES Microphone Windscreen Foam - Mic Cover Pop Filter Windshield &Protector for Blue Yeti, Yeti Pro Condenser Microphones (Black) ",
+      desc: "A nice extra to give the microphone a professional screen and look.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07CTBFSZS/ref=ppx_yo_dt_b_asin_title_o00_s02?ie=UTF8&psc=1",
+    },
+    {
+      title: "",
+      name: "",
+      desc: "",
+      img: "",
+      link: "",
+    },
+  ],
+  laptop: [
+    {
+      title: "Laptop",
+      name: "Apple Macbook Pro MJLT2LL/A 15-inch Laptop (2.5 GHz Intel Core i7 Processor, 16GB RAM, 512 GB Hard Drive, Mac OS X) (2015 version)",
+      desc: "Highly recommended laptop for the budget developer to work in the Apple Operating System.",
+      img: "",
+      link: "http://www.amazon.com",
+    },
+    {
+      title: "Case Cover",
+      name: "iLeadon Macbook Case",
+      desc: "A macbook pro cover to protect and cover up the apple logo that might be attractive for thieves.",
+      img: "",
+      link: "https://www.amazon.com/dp/B078GMKH9H/ref=twister_B078JFCV3C",
+    },
+    {
+      title: "Laptop Stand",
+      name: "Roost Laptop Stand",
+      desc: "A must-have for travel developers with neck strain or back pain who are health and posture conscious.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B01C9KG8IG/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1",
+    },
+    {
+      title: "Keyboard, bluetooth",
+      name: "Anker Ultra Compact Slim Profile Wireless Bluetooth Keyboard for iOS, Android, Windows and Mac with Rechargeable 6-Month Battery (White) ",
+      desc: "There's a newer version of this but it's a great bluetooth keyboard that feels sturdy.",
+      img: "",
+      link: "https://www.amazon.com/Anker-Wireless-Bluetooth-Keyboard-Rechargeable/dp/B00PIMKN1Q",
+    },
+    {
+      title: "Mouse, bluetooth",
+      name: "Logitech M720 Triathalon Multi-Device Wireless Mouse – Easily Move Text, Images and Files Between 3 Windows and Apple Mac Computers Paired with Bluetooth or USB, Hyper-Fast Scrolling, Black",
+      desc: "A sturdy bluetooth mouse that can connect to multiple devices.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B01LF37K80/ref=ppx_yo_dt_b_asin_title_o09_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Charger",
+      name: "MacBook Pro Charger 60W Power Adapter Magsafe 2 T-tip Style Connector Replacement MacBook Pro Charger for Apple MacBook Pro Retina 11 inch / 13 inch from Late 2012 up to mid 2016 White",
+      desc: "A better charger than the the mac one in my opinion with extra USB for charging your phone.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B079G86T9W/ref=ppx_od_dt_b_asin_title_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Portable Monitor",
+      name: "ASUS MB169B+ 15.6 Full HD 1920x1080 IPS USB Portable Monitor",
+      desc: "Instead of using an ipad, this extra monitor is a boon for productivity.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B013XFJKGI/ref=ppx_yo_dt_b_asin_title_o09_s01?ie=UTF8&psc=1",
+    },
+    {
+      title: "Heaphones, bluetooth",
+      name: "Kinivo BTH240 Bluetooth Headphones (Black, On-Ear, Wireless Music, Hands-Free Calling, Built-in Mic, Foldable, Memory Form Earpads, Travel Bag)",
+      desc: "When you don't want headphone helmet hair and wire free audio.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B00AWIPITS/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "External Hard Drive",
+      name: "WD 4TB Black My Passport Portable External Hard Drive - USB 3.0 - WDBYFT0040BBK-WESN",
+      desc: "I back up my life on this thing and still have not run out of space yet.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B01LQQH86A/ref=ppx_yo_dt_b_asin_title_o00_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "External Power Bank",
+      name: "88.8Wh|65Watts Portable Laptop Charger with AC Outlet, A Super Travel Portable Battery Pack & Power Bank for HP, Notebooks, MacBook, Laptops",
+      desc: "A nice extra power boost for your laptop and devices when you want to work outside.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B07P87JTVT/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "External Power Bank",
+      name: "Portable Charger Anker PowerCore 20100mAh - Ultra High Capacity Power Bank with 4.8A Output and PowerIQ Technology, External Battery Pack for iPhone, iPad & Samsung Galaxy & More (Black)",
+      desc: "A lightweight power boost for charging your phone on the go.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B00X5RV14Y/ref=ppx_yo_dt_b_asin_title_o01_s01?ie=UTF8&psc=1",
+    },
+    {
+      title: "Ethernet Adapter",
+      name: "AmazonBasics USB 3.0 to 10/100/1000 Gigabit Ethernet Internet Adapter",
+      desc: "Awesome in a pinch where there's no WiFi signal. (Like that could happen? It has to me.)",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B00M77HMU0/ref=ppx_od_dt_b_asin_title_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "",
+      name: "",
+      desc: "",
+      img: "",
+      link: "",
+    },
+  ],
+  phone_tools: [
+    {
+      title: "Phone, Primary",
+      name: "LG G6 H870DS 64GB Black, 5.7, Dual Sim, 4GB RAM, GSM Unlocked International Model, No Warranty",
+      desc: "A flagship phone from the past that boasts water resistance and with a price tag that won't burn a hole in your pocket.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B06ZZ5Z9S9/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Primary Phone Case",
+      name: "Spigen Tough Armor Designed for LG G6 Case (2017) / LG G6 Plus Case (2017) - Gunmetal",
+      desc: "With the extra savings from the budget found, this is a nice case with a kickstand.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B01MYAJT88/ref=ppx_yo_dt_b_asin_title_o07_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Phone, Secondary",
+      name: "Apple iPhone SE 64 GB Unlocked, Space Gray",
+      desc: "When you go to a country and need a backup phone.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B01DAJUDYK/ref=ppx_yo_dt_b_asin_title_o03_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "Secondary Phone Case",
+      name: "SPIDERCASE iPhone 5/5S/SE Waterproof Case, Full Body Protective Cover Rugged Dustproof Snowproof IP68 Certified Waterproof Case with Touch ID for iPhone 5S 5 SE (White&Clear)",
+      desc: "A case that makes your iPhone SE waterproof and almost indestructible.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B074GR8W2Q/ref=ppx_yo_dt_b_asin_title_o04_s00?ie=UTF8&psc=1",
+    },
+  ],
+  recording_tools: [
+    {
+      title: "",
+      name: "",
+      desc: "",
+      img: "",
+      link: "",
+    },
+  ],
+  travel_tools: [
+    {
+      title: "",
+      name: "Charles Schwab Debit Card",
+      desc: "",
+      img: "",
+      link: "",
+    },
+    {
+      title: "",
+      name: "Chase Sapphire Reserve Card",
+      desc: "",
+      img: "",
+      link: "",
+    },
+    {
+      title: "",
+      name: "Capital One Debit Card",
+      desc: "",
+      img: "",
+      link: "",
+    },
+    {
+      title: "Backpack Locks",
+      name: "TSA Luggage Locks (2 Pack) - 4 Digit Combination Steel Padlocks - Approved Travel Lock for Suitcases & Baggage - Black ",
+      desc: "You should always opt for a 4 digit lock over a 3 because how quickly they are to crack.",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B0114C1TM0/ref=ppx_yo_dt_b_asin_title_o04_s00?ie=UTF8&psc=1"
+    },
+    {
+      title: "",
+      name: "GoPro Hero 5",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Joby Gorilla Pod Action Video Tripod",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Airbnb",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Uber",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Garage Fit Wood Gym Rings, Wooden Gymnastic Rings, Fitness Rings, Exercise Rings, Gym Ring, Gymnast Rings, Gymnastics Rings",
+      desc: "",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B01LZYG9VB/ref=ppx_yo_dt_b_asin_title_o06_s00?ie=UTF8&psc=1",
+    },
+    {
+      title: "",
+      name: "Wahl Lithium Ion All-in-One Beard Trimmer Men’s Grooming Kit - Rechargeable Beard Trimmer, Hair Clipper & Electric Shavers – Model 9854-600 ",
+      desc: "",
+      img: "",
+      link: "https://www.amazon.com/gp/product/B001GQ3GEY/ref=ppx_yo_dt_b_asin_title_o09_s00?ie=UTF8&psc=1",
+    },
+  ],
+  software_tools: [
+    {
+      title: "",
+      name: "",
+      desc: "",
+      img: "",
+      link: "",
+    },
+  ],
+  web_development: [
+    {
+      title: "",
+      name: "Namecheap",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Google Drive",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Last Pass",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Free Code Camp",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Udemy",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "Front End Masters",
+      desc: "",
+      img: "",
+      link: ""
+    },
+    {
+      title: "",
+      name: "",
+      desc: "",
+      img: "",
+      link: ""
+    },
+  ],
+}
--- src/components/Gear/index.js
+++ src/components/Gear/index.js
@@ -1,9 +1,14 @@
+// LOCAL
+import { resources } from '../../assets'
+
 class Gear extends React.Component {
   render() {
     return (
       <main className="Gear">
-        <h1>Desktop</h1>
-        <h1>Laptop</h1>
+        <h1>Desktop Computer</h1>
+        {resources.desktop.map(item => <p>{item.title}: {item.name}</p>)}
+        <h1>Laptop Computer</h1>
+        {resources.laptop.map(item => <p>{item.title}: {item.name}</p>)}
       </main>
     )
   }
